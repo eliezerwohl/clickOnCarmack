3d50a96ab183e509c8ff952112a343f8f08efcda
eliezerwohl <eliezerwohl@gmail.com>
2015-11-16 12:41:36 -0500

    added class of john to everything, each is now resposive to a click in the js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-prompt('herro')
+// var john = document.getElementsByClassName("john");
+
+// john.addEvent("click, hooray");
+
+// function hooray() {
+//   prompt('hey soul sister');
+// 
+
+// document.getElementsByClassName("john").addEventListener("click", prompt("got it"));
+
+
+
+// get all of the delete buttons
+var john = document.getElementsByClassName("john");
+
+
+// loop over delete buttons and add event listener
+for(i = 0; i < john.length; i++){
+  john[i].addEventListener("click", hello);
+}
+
+
+// function to delete row
+function hello(){
+  prompt('sure plays a mean');
+}
