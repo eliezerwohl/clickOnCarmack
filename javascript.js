573bbdccabd365f4781eb316156b9bb247abff6b
eliezerwohl <eliezerwohl@gmail.com>
2015-11-16 15:21:48 -0500

    added the actual game, works

--- javascript.js
+++ javascript.js
@@ -1,26 +1,70 @@
+// get all of the delete buttons
+
+
+document.getElementById("myBtn").addEventListener("click", countdown);
+function countdown() {
+setTimeout(function(){ 
+prompt("you click on" + " " + counter + " " + "heads");   
+}, 4000);
+
+}
+
+var counter = 0;
+function countup() {
+counter = counter + 1;
+}
+
+
+
+// function countdown(){ 
+//     prompt("sucka!");   
+// }, 5000);
+
 // var john = document.getElementsByClassName("john");
 
-// john.addEvent("click, hooray");
 
-// function hooray() {
-//   prompt('hey soul sister');
-// 
-
-// document.getElementsByClassName("john").addEventListener("click", prompt("got it"));
+// // loop over delete buttons and add event listener
+// john.addEventListener("click", function(){ alert("Hello World!"); });
 
 
-
-// get all of the delete buttons
-var john = document.getElementsByClassName("john");
-
-
-// loop over delete buttons and add event listener
-for(i = 0; i < john.length; i++){
-  john[i].addEventListener("click", hello);
-}
+// // function to delete row
+// function hello(){
+//   prompt('sure plays a mean');
 
 
-// function to delete row
-function hello(){
-  prompt('sure plays a mean');
-}
+
+// var counter = 0;
+// function countup() {
+//   counter = counter + 1;
+//   }
+
+
+// function countdown() {
+//   setTimeout(function countandgive()
+//     { 
+//     alert("TIME-OUT! You got " +counter+ " Teams!");
+     
+//      }, 20000);
+
+// }
+
+
+// // get all of the delete buttons
+// var john = document.getElementsByClassName("john");
+// var buttonGo = document.getElementsByClassName("buttonGo")
+
+
+
+
+// // loop over delete buttons and add event listener
+// for(i = 0; i < buttonGo.length; i++){
+//   buttonGo[i].addEventListener("click", prompt('this is'));
+// }
+
+
+// // function to delete row
+// function hello(){
+//   prompt('sure plays a mean');
+// }
+
+
