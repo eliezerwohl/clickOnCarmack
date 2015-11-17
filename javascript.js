50750c9fd9c383dabb40cefc410e066dac57e30e
eliezerwohl <eliezerwohl@gmail.com>
2015-11-17 11:03:17 -0500

    ready to deploy to heroku

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ document.getElementById("myBtn").addEventListener("click", countdown);
 function countdown() {
 setTimeout(function(){ 
 prompt("you click on" + " " + counter + " " + "heads");   
-}, 4000);
+}, 20000);
 
 }
 
@@ -15,56 +15,15 @@ counter = counter + 1;
 }
 
 
+function handleClick(e)
+{
+    countup();
+    e.target.removeEventListener('click', handleClick);
+}
 
-// function countdown(){ 
-//     prompt("sucka!");   
-// }, 5000);
-
-// var john = document.getElementsByClassName("john");
-
-
-// // loop over delete buttons and add event listener
-// john.addEventListener("click", function(){ alert("Hello World!"); });
-
-
-// // function to delete row
-// function hello(){
-//   prompt('sure plays a mean');
-
-
-
-// var counter = 0;
-// function countup() {
-//   counter = counter + 1;
-//   }
-
-
-// function countdown() {
-//   setTimeout(function countandgive()
-//     { 
-//     alert("TIME-OUT! You got " +counter+ " Teams!");
-     
-//      }, 20000);
-
-// }
-
-
-// // get all of the delete buttons
-// var john = document.getElementsByClassName("john");
-// var buttonGo = document.getElementsByClassName("buttonGo")
-
-
-
-
-// // loop over delete buttons and add event listener
-// for(i = 0; i < buttonGo.length; i++){
-//   buttonGo[i].addEventListener("click", prompt('this is'));
-// }
-
-
-// // function to delete row
-// function hello(){
-//   prompt('sure plays a mean');
-// }
-
+var images = document.querySelectorAll('img'); 
+for (var i = 0; i < images.length; i++) 
+{
+    images[i].addEventListener('click', handleClick);
+}
 
